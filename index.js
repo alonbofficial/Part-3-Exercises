9f9d00c3d65ac5cbd47890a16ac4198964e38ccf
alonbofficial <55811049+alonbofficial@users.noreply.github.com>
2022-09-15 21:47:14 +0300

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people\n ${new Date()}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    const found = persons.find(person => person.id === id)
+    if(found){
+        response.json(found)
+    }else{
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
